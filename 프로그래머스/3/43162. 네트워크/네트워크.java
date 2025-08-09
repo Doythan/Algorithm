a3e219de4fdff2c9e3068710f94cf4edf81a7078
Do gyeongwon (도경원) <won03289@gmail.com>
2025-08-09 17:33:52 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,21 +2,20 @@ class Solution {
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
         int networkCount = 0;
-
+        
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+            if(!visited[i]) {
                 dfs(i, computers, visited);
-                networkCount++; // 한 번 DFS 탐색 = 하나의 네트워크
+                networkCount++;
             }
         }
-
-        return networkCount;
+        return networkCount++;
     }
-
-    private void dfs(int current, int[][] computers, boolean[] visited) {
+    
+    public void dfs(int current, int[][] computers, boolean[] visited) {
         visited[current] = true;
-
-        for (int i = 0; i < computers.length; i++) {
+        
+        for (int i = 0; i < computers[current].length; i++) {
             if (computers[current][i] == 1 && !visited[i]) {
                 dfs(i, computers, visited);
             }
