632c3a75174d53f7e7a8a2d28e68b2971e13df1f
Do gyeongwon (도경원) <won03289@gmail.com>
2025-08-29 19:48:33 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2356 ms, Memory: 619072 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+import org.w3c.dom.Node;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		Deque<Node> mydeque = new LinkedList<>();
+		for(int i=0; i<N; i++) {
+			int now = Integer.parseInt(st.nextToken());
+			// 새로운 값이 들어올 때마다 정렬 대신 현재 수보다 큰 값을 덱에서 제거해 시간 복잡도를 줄인다.
+			while(!mydeque.isEmpty() && mydeque.getLast().value > now) {
+				mydeque.removeLast();
+			}
+			mydeque.addLast(new Node(now, i));
+			// 범위에서 벗어난 값은 덱에서 제거
+			if(mydeque.getFirst().index <= i-L) {
+				mydeque.removeFirst();
+			}
+			bw.write(mydeque.getFirst().value + " ");
+		}
+		bw.flush();
+		bw.close(); 
+	}
+	
+	static class Node {
+		public int value;
+		public int index;
+		
+		Node(int value, int index) {
+			this.value = value;
+			this.index = index;
+		}
+	}
+}
