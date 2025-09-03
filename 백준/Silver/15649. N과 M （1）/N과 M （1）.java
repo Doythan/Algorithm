ee2b9d66d75f77f08cc9209542b411ba9f5acab0
Do gyeongwon (도경원) <won03289@gmail.com>
2025-09-04 00:09:38 +0900

    [Silver III] Title: N과 M (1), Time: 1644 ms, Memory: 57588 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,39 +3,38 @@ import java.io.*;
 
 public class Main {
 	static int N, M;
-	static boolean[] V;  // 숫자 사용 여부 저장하기
-	static int[] S;      // 수 정보 저장하기 
+	static int[] numbers;
+	static boolean[] visited;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		S = new int[M];
-		V = new boolean[N];
-		backtracking(0); 
+		numbers = new int[M];
+		visited = new boolean[N];
+		back(0);
 	}
 	
-	private static void backtracking(int length) {
-		if (length == M) {					// 길이가 M인 수열이 만들어진 경우  
+	private static void back(int length) {
+		if (length == M) {
 			printArray();
 			return;
 		}
-		for(int i=0; i<N; i++) {
-			if(!V[i]) {
-				V[i] = true;                // 수 사용 저장하기 
-				S[length] = i;              // 수열에 수 사용하기 
-				backtracking(length + 1);   
-				V[i] = false;				// 수 반납 저장하기 
+		
+		for (int i=0; i<N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				numbers[length] = i;
+				back(length + 1);
+				visited[i] = false;
 			}
 		}
 	}
 	
 	private static void printArray() {
 		for(int i=0; i<M; i++) {
-			System.out.print(S[i] + 1 + " ");
+			System.out.print(numbers[i] + 1 + " ");
 		}
 		System.out.println();
 	}
-	
-	
 }
