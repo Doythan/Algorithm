f8baf916ac3e702662cc53ed60f0e7a033bdb0fd
Do gyeongwon (도경원) <won03289@gmail.com>
2025-09-17 20:23:01 +0900

    [level 2] Title: 전화번호 목록, Time: 370.82 ms, Memory: 272 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        for(String num : phone_book) set.add(num);
-        
-        for(String num : phone_book) {
+        Set<String> number = new HashSet<>();
+        for(String phonenumbers : phone_book) number.add(phonenumbers);
+        for(String phonenumbers : phone_book) {
             StringBuilder sb = new StringBuilder();
-            for(int i=0 ; i<num.length() - 1; i++) {
-                sb.append(num.charAt(i));
-                if(set.contains(sb.toString())) return false;
+            for(int i=0; i<phonenumbers.length()-1; i++) {
+                sb.append(phonenumbers.charAt(i));
+                if(number.contains(sb.toString())) return false;
             }
         }
         return true;
