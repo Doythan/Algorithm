27e7e140e55e9671fd05d961358a5b7b1af4c714
Do gyeongwon (도경원) <won03289@gmail.com>
2025-09-17 21:01:10 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        
+        for(String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+        
+        for (int cnt : map.values()) {
+            answer *= (cnt + 1);
+        }
+        
+        return answer - 1;
+    }
+}
