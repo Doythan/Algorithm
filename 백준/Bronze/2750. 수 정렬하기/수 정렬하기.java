4124bf6b25f32fa35b1a34817eca21a3071f5987
Do gyeongwon (도경원) <won03289@gmail.com>
2025-11-16 15:39:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 164 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,26 +1,27 @@
 import java.util.*;
-
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[] A = new int[N];
-		for(int i=0; i<N; i++) {
-			A[i] = sc.nextInt();
+		for (int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
 		}
 		
-		for(int i=0; i<N-1; i++) {
-			for(int j=0; j<N-1; j++) {
+		int temp = 0;
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N-1; j++) { 
 				if (A[j] > A[j+1]) {
-					int temp = A[j];
+					temp = A[j];
 					A[j] = A[j+1];
 					A[j+1] = temp;
 				}
 			}
 		}
-		for(int i=0; i<N; i++) {
+		
+		for (int i=0; i<N; i++) {
 			System.out.println(A[i]);
 		}
 	}
